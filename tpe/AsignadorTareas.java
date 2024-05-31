8d8da5ad1064abdf3d858a7e0ddbabf08c2e44b3
victoria ledesma <vickyledesma988@gmail.com>
2024-05-31 17:44:20 -0300

    agrego commits :p

--- tpe/AsignadorTareas.java
+++ tpe/AsignadorTareas.java
@@ -1,39 +1,38 @@
 package tpe;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class AsignadorTareas {
 
     public static void asignarTareas(HashMap<String, Tarea> tareas,HashMap<String, Procesador> procesadores,int tiempoX) {
-        List<Procesador> mejorAsignacion = new ArrayList<>(); 
-        List<Tarea> tareasSinAsignar = new ArrayList<>(tareas.values());
-        int mejorTiempoFinal = 0;
-        asignarTareasRecursivo(tareasSinAsignar, mejorAsignacion, tiempoX, procesadores, tareas, mejorTiempoFinal);
+        List<Procesador> mejorAsignacion = new LinkedList<>(); 
+        List<Tarea> tareasSinAsignar = new LinkedList<>(tareas.values());
+        int mejorTiempoFinal = Integer.MAX_VALUE;
+        int contador = 0;
+        asignarTareasRecursivo(contador, tareasSinAsignar, mejorAsignacion, tiempoX, procesadores, tareas, mejorTiempoFinal);
     }
 
-    private static void asignarTareasRecursivo(List<Tarea> tareasSinAsignar, List<Procesador> mejorAsignacion, int tiempoX, HashMap<String, Procesador> procesadores, HashMap<String, Tarea> tareas, int mejorTiempoFinal) {
+    private static void asignarTareasRecursivo(int contador,List<Tarea> tareasSinAsignar, List<Procesador> mejorAsignacion, int tiempoX, HashMap<String, Procesador> procesadores, HashMap<String, Tarea> tareas, int mejorTiempoFinal) {
+        contador++;
         if (tareasSinAsignar.isEmpty()) {
             int tiempoFinal = calcularTiempoFinal(procesadores);
             if (tiempoFinal < mejorTiempoFinal) {
                 mejorTiempoFinal = tiempoFinal;
                 mejorAsignacion.clear();
                 for (Procesador p : procesadores.values()) {
-                    mejorAsignacion.add(p); 
+                    mejorAsignacion.add(new Procesador(p)); 
                 }
             }
-            return;
         }
-        Tarea tarea = tareasSinAsignar.get(0); /// ??????
-
+        else {}
+        Tarea tarea = tareasSinAsignar.get(0); 
         for (Procesador procesador : procesadores.values()) {
             if (procesador.cumpleRestricciones(tarea,tiempoX)) {
                 procesador.asignarTarea(tarea);
                 tareasSinAsignar.remove(tarea);
 
-                asignarTareasRecursivo(tareasSinAsignar, mejorAsignacion, tiempoX, procesadores,tareas,mejorTiempoFinal);
+                asignarTareasRecursivo(contador,tareasSinAsignar, mejorAsignacion, tiempoX, procesadores,tareas,mejorTiempoFinal);
 
                 procesador.removerTarea(tarea);
                 tareasSinAsignar.add(0, tarea);
--- tpe/Procesador.java
+++ tpe/Procesador.java
@@ -1,8 +1,4 @@
 package tpe;
-
-import java.sql.Date;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 
 public class Procesador {
@@ -12,6 +8,7 @@ public class Procesador {
     private int año_funcionamiento;
     private List<Tarea> tareas_asignadas;
     private int tiempo_ejecucion;
+    private int cantCriticas;
 
     
     public Procesador(String id_procesador, String codigo_procesador, boolean esta_refrigerado, Integer anio,List<Tarea> tareas_asignadas, int tiempo_ejecucion) {
@@ -22,7 +19,10 @@ public class Procesador {
         this.tareas_asignadas = tareas_asignadas;
         tiempo_ejecucion = 0;
     }
-                        //getter y setter
+    public Procesador(Procesador p) {
+        //TODO Auto-generated constructor stub
+    }
+    //getter y setter
     public List<Tarea> getTareas_asignadas() {
         return tareas_asignadas;
     }
@@ -35,6 +35,15 @@ public class Procesador {
     public String getCodigo_procesador() {
         return codigo_procesador;
     }
+    public void setTiempo_ejecucion(int tiempo_ejecucion) {
+        this.tiempo_ejecucion = tiempo_ejecucion;
+    }
+    public int getCantCriticas() {
+        return cantCriticas;
+    }
+    public void setCantCriticas(int cantCriticas) {
+        this.cantCriticas = cantCriticas;
+    }
     public boolean isEsta_refrigerado() {
         return esta_refrigerado;
     }
@@ -42,29 +51,9 @@ public class Procesador {
         return año_funcionamiento;
     }
    
-    public int tiempo_ejecucion(Procesador procesador){
-        Iterator<Tarea> iterator = procesador.getTareas_asignadas().iterator();
-        while(iterator.hasNext()){
-            Tarea tarea = iterator.next();
-            tiempo_ejecucion += tarea.getTiempo_ejecucion();
-        }
-        return tiempo_ejecucion;
-    }
 
     public boolean cumpleRestricciones(Tarea tarea, int tiempoX) {
-        int criticasAsignadas = 0;
-        int tiempomaximo = this.getTiempo_ejecucion();
-        Iterator<Tarea> iterator = this.getTareas_asignadas().iterator();
-        while (iterator.hasNext()) {
-            Tarea tareaAsignada = iterator.next();
-            if (tareaAsignada.isEs_critica()) {
-                criticasAsignadas++;
-            }
-        }
-        if (tarea.isEs_critica()) {
-            criticasAsignadas++;
-        }
-        if (criticasAsignadas < 2 && this.isEsta_refrigerado() && tiempomaximo < tiempoX) {
+        if (cantCriticas < 2 && this.isEsta_refrigerado() && this.getTiempo_ejecucion() < tiempoX) {
             return true;
         }
         return false;
@@ -72,10 +61,17 @@ public class Procesador {
     
     public void asignarTarea(Tarea tarea){
         tareas_asignadas.add(tarea);
-
+        tiempo_ejecucion += tarea.getTiempo_ejecucion();
+        if(tarea.isEs_critica()){
+            cantCriticas++;
+        }
     }
     public void removerTarea(Tarea tarea) {
         tareas_asignadas.remove(tarea);
+        tiempo_ejecucion -= tarea.getTiempo_ejecucion();
+        if (tarea.isEs_critica()) {
+            cantCriticas--;        
+        }
     }
 }
 
